94f7ce3c6af0ec115982a5fc4dc88df53bbf1620
Petr Malecha <petr.malecha@mfcc.cz>
2017-03-30 12:10:49 +0200

    init datatypes

---
+++ config/module.config.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Adminaut\Datatypes;
+
+return [
+];
---
+++ src/Text.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Adminaut\Datatypes;
+
+/**
+ * Class Text
+ * @package Adminaut\Datatypes
+ */
+class Text extends \Zend\Form\Element\Text
+{
+
+}
