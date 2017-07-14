7b9b4ee0c686ca24ea40e95492d782097c310a53
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-07-14 15:34:15 +0200

    Temporary section delimiter - ticket:#24460

--- src/StaticElement.php
+++ src/StaticElement.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Adminaut\Datatype;
+
 use Traversable;
 
 /**
@@ -11,5 +12,11 @@ class StaticElement extends \TwbBundle\Form\Element\StaticElement
 {
     use Datatype {
         setOptions as datatypeSetOptions;
+    }
+
+    // todo: temporary show delimiter between sections
+    public function getValue()
+    {
+        return '<b>----- ' . $this->getLabel() . ' ------</b>';
     }
 }
