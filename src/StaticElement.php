f6d75fa1e930dc58cc2adb44acb72c322ca6eb18
Josef Soušek <josef.sousek@mfcc.cz>
2017-10-02 14:27:27 +0200

    Static Element - update zf2

--- src/StaticElement.php
+++ src/StaticElement.php
@@ -14,9 +14,34 @@ class StaticElement extends \TwbBundle\Form\Element\StaticElement
         setOptions as datatypeSetOptions;
     }
 
-    // todo: temporary show delimiter between sections
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @param array|Traversable $options
+     */
+    public function setOptions($options)
+    {
+        if(isset($options['label'])) {
+            $this->title = $options['label'];
+            unset($options['label']);
+        }
+
+        if(isset($options['title'])) {
+            $this->title = $options['title'];
+        }
+
+        $this->datatypeSetOptions($options);
+    }
+
+    /**
+     * todo: temporary show delimiter between sections
+     * @return string
+     */
     public function getValue()
     {
-        return '<b>----- ' . $this->getLabel() . ' ------</b>';
+        return '<h3 class="static-element">' . $this->title . '</h3>';
     }
 }
